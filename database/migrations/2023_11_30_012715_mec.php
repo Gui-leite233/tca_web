c37c009992edce50ca45b8ab34d5593b7dcb890b
guilherme <89623564+Gui-leite233@users.noreply.github.com>
2023-11-29 22:27:58 -0300

    tca_web

--- database/migrations/2023_11_30_012715_mec.php
+++ database/migrations/2023_11_30_012715_mec.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Informatica extends Migration
+class Mec extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class Informatica extends Migration
      */
     public function up()
     {
-        Schema::create('informatica', function (Blueprint $table) {
+        Schema::create('mec', function (Blueprint $table) {
             $table->id();
-            
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class Informatica extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('informatica');
+        Schema::dropIfExists('mec');
     }
 }
