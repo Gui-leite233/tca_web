e97f41ac04af453436abefad959c3b153cfbc842
guilherme <89623564+Gui-leite233@users.noreply.github.com>
2023-12-10 20:38:33 -0300

    tca_web
    progresso no cadastro;
    alterar a capacidade máxima de caracteres de "descrição";
    definir como funcionará o cadastro de coordenador

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -40,7 +40,7 @@ class CursoController extends Controller
     {
         $obj = new Curso();
         $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
-        $obj->descricao = mb_strtoupper($request->email, 'UTF-8');
+        $obj->descricao = mb_strtoupper($request->descricao, 'UTF-8');
         $obj->duracao = mb_strtoupper($request->duracao, 'UTF-8');
         $obj->save();
 
